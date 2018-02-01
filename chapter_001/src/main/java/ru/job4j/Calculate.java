52ec5bf112cb031fcc724501ee00e1aacb04fccd
KonstantinKovtun <kovtun.kostya@gmail.com>
2018-02-01 22:02:00 +0200

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,11 +1,29 @@
 package ru.job4j;
 
 /**
-*  онструктор, выводит строки в консоль.
-* @param arg - arg.
+ * Calculate.
+ *
+ * @author Kovtun Konstantin (kovtun.kostya@gmai.com)
+ * @version $Id$
+ * @since 0.1
+ *  онструктор, выводит строки в консоль.
+ * @param arg - arg.
 */
 public class Calculate {
+    /**
+     * Main.
+     * @param args - args.
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+	
+    /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
     }
 }
