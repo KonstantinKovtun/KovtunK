2493840f8b59bd189689d590d18985f31168d3e3
KonstantinKovtun <kovtun.kostya@gmail.com>
2018-02-13 23:23:30 +0200

    1.3. JavaDoc. Code convention [#4087].

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,25 +1,58 @@
 package ru.job4j.calculator;
+/**
+ * Class Calculator makes some mathematical operations.
+ *
+ * @author Kovtun Konstantin (kovtun.kostya@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 
 public class Calculator {
-    
+    /**
+	 * A variable of mathematical operations value.
+	 */
 	public double result;
 	
+	/**
+	 * add. Method summarize two values.
+	 * @param first, first value.
+	 * @param second, second value.
+	 */
 	public void add(double first, double second) {
 		this.result = first + second;
 	}
 	
+	/**
+	 * subtract. Subtracts one values from the other one value.
+	 * @param first, first value.
+	 * @param second, second value.
+	 */
 	public void subtract(double first, double second) {
 		this.result = first - second;
 	}
 	
+	/**
+	 * divide. Divides one number by another.
+	 * @param first, first value.
+	 * @param second, second value.
+	 */
 	public void div(double first, double second) {
 		this.result = first / second;
 	}
 	
+	/**
+	 * multiple. Multiplies one number on another.
+	 * @param first, first value.
+	 * @param second, second value.
+	 */
 	public void multiple(double first, double second) {
 		this.result = first * second;
 	}
 	
+	/**
+	 * getResult. Ger the value of variable result.
+	 * @return result.
+	 */
 	public double getResult() {
 		return this.result;
 	}
--- chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -4,9 +4,18 @@ import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-
+/**
+ * Test class Calculator.
+ *
+ * @author Kovtun Konstantin (kovtun.kostya@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class CalculatorTest {
-    @Test
+    
+    /**
+     * Test whenAddOnePlusOneThenTwo.
+     */ @Test
     public void whenAddOnePlusOneThenTwo() {
         Calculator calc = new Calculator();
         calc.add(1D, 1D);
@@ -15,7 +24,9 @@ public class CalculatorTest {
         assertThat(result, is(expected));
     }
 	
-	@Test
+    /**
+     * Test whenSubtractOneFromOneThenZero.
+     */ @Test
     public void whenSubtractOneFromOneThenZero() {
         Calculator calc = new Calculator();
         calc.subtract(1D, 1D);
@@ -24,7 +35,9 @@ public class CalculatorTest {
         assertThat(result, is(expected));
     }
 	
-	@Test
+    /**
+     * Test whenDivOneToOneThenThree.
+     */ @Test
     public void whenDivOneToOneThenThree() {
         Calculator calc = new Calculator();
         calc.div(6D, 2D);
@@ -33,7 +46,9 @@ public class CalculatorTest {
         assertThat(result, is(expected));
 	}
 	
-	@Test
+    /**
+     * Test whenMultipleOneOnOneThenTwelve.
+     */ @Test
     public void whenMultipleOneOnOneThenTwelve() {
         Calculator calc = new Calculator();
         calc.multiple(6D, 2D);
