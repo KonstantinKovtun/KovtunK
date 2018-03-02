011279de7087bcb145bba620e564ae0232d13f64
KonstantinKovtun <kovtun.kostya@gmail.com>
2018-03-03 00:09:10 +0200

    4.4. Построить пирамиду в псевдографике. [#4412]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -0,0 +1,92 @@
+package ru.job4j.loop;
+
+/**
+ * Class Paint builds a pyramid.
+ * @author Kovtun Konstantin (kovtun.kostya@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Paint {
+    /**
+     * The method builds the pyramid.
+     * @param height, the value of a height.
+     * @return pyramid.
+     */
+    public String pyramid(int height) {
+
+        StringBuilder screen = new StringBuilder();
+
+        int weight = 2 * height - 1;
+
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != weight; column++) {
+                if (row >= height - column - 1 && row + height - 1 >= column) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+
+    /**
+     * The method builds the right triangle.
+     * @param height, the value of a height.
+     * @return right triangle.
+     */
+    public String rightTrl(int height) {
+        // Буфер для результата.
+        StringBuilder screen = new StringBuilder();
+        // ширина будет равна высоте.
+        int weight = height;
+        // внешний цикл двигается по строкам.
+        for (int row = 0; row != height; row++) {
+            // внутренний цикл определяет положение ячейки в строке.
+            for (int column = 0; column != weight; column++) {
+                // если строка равна ячейки, то рисуем галку.
+                // в данном случае строка определяем, сколько галок будет на строке
+                if (row >= column) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            // добавляем перевод строки.
+            screen.append(System.lineSeparator());
+        }
+        // Получаем результат.
+        return screen.toString();
+    }
+
+    /**
+     * The method builds the left triangle.
+     * @param height, the value of a height.
+     * @return left triangle.
+     */
+    public String leftTrl(int height) {
+        StringBuilder screen = new StringBuilder();
+        int weight = height;
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != weight; column++) {
+                if (row >= weight - column - 1) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+
+    public static void main(String[] args) {
+        final int HEIGHT = 3;
+
+        Paint paint = new Paint();
+
+        System.out.println(paint.pyramid(HEIGHT));
+
+    }
+}
