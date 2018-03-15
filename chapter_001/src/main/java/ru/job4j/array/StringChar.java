a8dbc7d2b413b7cad4d45ee1730e3d3cfa52274f
KonstantinKovtun <kovtun.kostya@gmail.com>
2018-03-15 07:42:58 +0200

    Задание для теста

---
+++ chapter_001/src/main/java/ru/job4j/array/StringChar.java
@@ -0,0 +1,53 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+/**
+ * Class StringChar searches an element in array by index.
+ * @author Kovtun Konstantin (kovtun.kostya@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class StringChar {
+    /**
+     * back. Finds an element in array.
+     * @param origin, name of array.
+     * @param sub, name of array.
+     * @return boolean.
+     */
+    boolean contains(String origin, String sub) {
+        char[] chArrayOrigin = origin.toCharArray();
+        char[] chArraySub = sub.toCharArray();
+        int count = 0;
+
+        for (int i = 0; i < chArrayOrigin.length; i++) {
+            for (int j = 0; j < chArraySub.length; j++) {
+                if (chArrayOrigin[i] == chArraySub[j]) {
+                    if (chArraySub.length != j + 1) {
+                        if (chArrayOrigin[i + 1] == chArraySub[j + 1]) {
+                            count++;
+                            break;
+                        }
+                    } else {
+                        count++;
+                        break;
+                    }
+                    //count++;
+                    //break;
+                }
+            }
+
+            if (count == chArraySub.length) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String originStroka = "Пиbвет люди, как там на Земле ?";
+        String subStroka = "ивет";
+        StringChar stringChar = new StringChar();
+        System.out.println(stringChar.contains(originStroka, subStroka));
+        //System.out.println("Array : " + Arrays.toString(stringChar.contains(originStroka, subStroka)));
+    }
+}
