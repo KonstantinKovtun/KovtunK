9e41a13e8ed77f828874c46c52792b146ae86471
KonstantinKovtun <kovtun.kostya@gmail.com>
2018-04-11 23:06:53 +0300

    Вопросы. [#226]

---
+++ chapter_001/src/main/java/ru/job4j/array/SortArray.java
@@ -0,0 +1,34 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+/**
+ * Class SortArray creates new array and then sort it.
+ * @author Kovtun Konstantin (kovtun.kostya@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class SortArray {
+    /**
+     * insertElements. Fill the elements of array.
+     * @param array1, name of array.
+     * @param array2, name of array.
+     */
+    public int[] insertElements(int[] array1, int[] array2) {
+        int[] ara3 = new int[array1.length + array2.length];
+
+        System.arraycopy(array1, 0, ara3, 0, array1.length);
+        System.arraycopy(array2, 0, ara3, ara3.length / 2, array2.length);
+        Arrays.sort(ara3);
+
+        return ara3;
+    }
+
+    public static void main(String[] args) {
+        int[] ara1 = {4, -2, 0};
+        int[] ara2 = {5, -1, 28, -100};
+
+        SortArray sortArray = new SortArray();
+
+        System.out.println(Arrays.toString(sortArray.insertElements(ara1, ara2)));
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/SortArrayTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test class SortArrayTest
+ *
+ * @author Kovtun Konstantin (kovtun.kostya@gmai.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class SortArrayTest {
+    /**
+     * Test whenOneArrayPlusTwoArrayThenThirdArraySorted.
+     */
+    @Test
+    public void whenOneArrayPlusTwoArrayThenThirdArraySorted() {
+        SortArray sortArray = new SortArray();
+        int[] ara1 = {4, -2, 0};
+        int[] ara2 = {5, -1, 28, -100};
+        int[] result = sortArray.insertElements(ara1, ara2);
+        int[] expected = {-100, -2, -1, 0, 4, 5, 28};
+        assertThat(result, is(expected));
+    }
+}
