9bdde5e9ccab1bd5b7e0b2fca0d0f911a0a36613
KonstantinKovtun <kovtun.kostya@gmail.com>
2018-07-18 21:27:30 +0300

    Контрольные вопросы часть 1. Базовый синтаксис.[#47975]

--- chapter_001/src/main/java/ru/job4j/array/SortArray.java
+++ chapter_001/src/main/java/ru/job4j/array/SortArray.java
@@ -14,22 +14,54 @@ public class SortArray {
      * @param array2, name of array.
      */
     public int[] insertElements(int[] array1, int[] array2) {
-        int[] ara3 = new int[array1.length + array2.length];
-
+    /*  int[] ara3 = new int[array1.length + array2.length];
         System.arraycopy(array1, 0, ara3, 0, array1.length);
         System.arraycopy(array2, 0, ara3, ara3.length / 2, array2.length);
-        return ara3;
+
+     */
+        int[] ara = {1, 4, 3, 2};
+        int temp;
+
+        if (ara[0] > ara[1]) {
+            temp = ara[0];
+            ara[0] = ara[1];
+            ara[1] = temp;
+        } else {
+            if (ara[1] > ara[2]) {
+                temp = ara[1];
+                ara[1] = ara[2];
+                ara[2] = temp;
+            }
+        }
+        if (ara[2] > ara[3]) {
+            temp = ara[2];
+            ara[2] = ara[3];
+            ara[3] = temp;
+        }
+        if (ara[0] > ara[1]) {
+            temp = ara[0];
+            ara[0] = ara[1];
+            ara[1] = temp;
+        } else {
+            if (ara[1] > ara[2]) {
+                temp = ara[1];
+                ara[1] = ara[2];
+                ara[2] = temp;
+            }
+        }
+        if (ara[2] > ara[3]) {
+            temp = ara[2];
+            ara[2] = ara[3];
+            ara[3] = temp;
+        }
+        return ara;
     }
+
     public static void main(String[] args) {
- /*
-        int[] ara1 = {4, -2, 0};
-        int[] ara2 = {5, -1, 28, -100};
-*/
         int[] ara1 = {1, 2};
         int[] ara2 = {3, 4};
 
         SortArray sortArray = new SortArray();
-
         System.out.println(Arrays.toString(sortArray.insertElements(ara1, ara2)));
     }
 }
