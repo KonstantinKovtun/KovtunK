5c2967479ec876268dcb6fe7c26375c016f7eee3
KonstantinKovtun <kovtun.kostya@gmail.com>
2018-07-31 07:17:50 +0300

    1. Используя класс ConsoleInput в классе StartUI обеспечить полноценную работу всего приложения [#784]

--- chapter_002/src/main/java/ru/job4j/tracker/Input.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Input.java
@@ -1,4 +1,8 @@
 package ru.job4j.tracker;
 
-public class Input {
+import java.util.Arrays;
+
+public interface Input {
+    String ask(String question);
+    //все модификаторы доступа в интерфейса по умолчанию public
 }
--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,20 +2,132 @@ package ru.job4j.tracker;
 
 import java.util.Arrays;
 
-//Отображение данных, наше меню
+/**
+ * @version $Id$
+ * @since 0.1
+ */
 public class StartUI {
-    public static void main(String[] args) {
 
-        Tracker tracker = new Tracker();
-        tracker.add(new Item("19", "Name1", "desc1", 121));
-        tracker.add(new Item("29", "Name2", "desc2", 122));
-        tracker.add(new Item("39", "Name3", "desc3", 123));
-        tracker.add(new Item("49", "Name4", "desc4", 124));
-        tracker.add(new Item("59", "Name5", "desc5", 125));
+    /**
+     * Константа меню для добавления новой заявки.
+     */
+    private static final String ADD = "0";
 
-        tracker.delete(("19"));
-        for (Item item : tracker.findAll()) {
-            System.out.println(item.getId() + " " + item.getName() + " " + item.getDesc());
+    /**
+     * Константа получения списка всех заявок.
+     */
+    private static final String SHOW = "1";
+
+    /**
+     * Константа для редактирования заявки.
+     */
+    private static final String EDIT = "2";
+
+    /**
+     * Константа для удаления заявки.
+     */
+    private static final String DELETE = "3";
+
+    /**
+     * Константа для получения заявки по id.
+     */
+    private static final String FINDBYID = "4";
+
+    /**
+     * Константа для получения заявки по имени.
+     */
+    private static final String FINDBYNAME = "5";
+
+    /**
+     * Константа для выхода из цикла.
+     */
+    private static final String EXIT = "6";
+
+    /**
+     * Получение данных от пользователя.
+     */
+    private final Input input;
+
+    /**
+     * Хранилище заявок.
+     */
+    private final Tracker tracker;
+
+    /**
+     * Конструтор инициализирующий поля.
+     *
+     * @param input   ввод данных.
+     * @param tracker хранилище заявок.
+     */
+    public StartUI(Input input, Tracker tracker) {
+        this.input = input;
+        this.tracker = tracker;
+    }
+
+    /**
+     * Основой цикл программы.
+     */
+    public void init() {
+        boolean exit = false;
+        while (!exit) {
+            this.showMenu();
+            String answer = this.input.ask("Введите пункт меню : ");
+            if (ADD.equals(answer)) {
+                //добавление заявки вынесено в отдельный метод.
+                this.createItem();
+            } else if (SHOW.equals(answer)) {
+                //получения списка всех заявок вынесено в отдельный метод.
+                this.findAllItems();
+            } else if (EDIT.equals(answer)) {
+                this.editItem();
+            } else if (EXIT.equals(answer)) {
+                exit = true;
+            }
         }
+    }
+
+    /**
+     * Метод реализует добавленяи новый заявки в хранилище.
+     */
+    private void createItem() {
+        System.out.println("------------ Добавление новой заявки --------------");
+        String name = this.input.ask("Введите имя заявки :");
+        String desc = this.input.ask("Введите описание заявки :");
+        Item item = new Item("123", name, desc, 131232); //String id, String name, String desc, long create
+        this.tracker.add(item);
+        System.out.println("------------ Новая заявка с getId : " + item.getId() + "-----------");
+    }
+
+    /**
+     * Метод реализует получение списка заявок.
+     */
+    private void findAllItems() {
+        for (Item items : this.tracker.findAll()) {
+            System.out.println("Items' name : " + items.getName() + " " + "items' id : " + items.getId());
+        }
+    }
+
+    private void editItem() {
+        Item item = this.tracker.add(new Item("123", "name", "desc", 131232));
+
+        for (int i = 0; i < tracker.findAll().length; i++) {
+            if (tracker.findAll()[i].equals(item.getId())) {
+                this.tracker.replace(item.getId(), item);
+            }
+        }
+    }
+
+    private void showMenu() {
+        System.out.println("Меню.");
+        // добавить остальные пункты меню.
+    }
+
+    /**
+     * Запускт программы.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        new StartUI(new ConsoleInput(), new Tracker()).init();
     }
 }
