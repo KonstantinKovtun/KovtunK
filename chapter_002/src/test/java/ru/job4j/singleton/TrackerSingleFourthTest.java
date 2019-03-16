c88b3f3435c232a4939db5a7f02c6c0f2a939b53
KonstantinKovtun <kovtun.kostya@gmail.com>
2019-03-16 03:43:11 +0200

    1. Singleton. [#94606]2

---
+++ chapter_002/src/test/java/ru/job4j/singleton/TrackerSingleFourthTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.singleton;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.tracker.Item;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class TrackerSingleFourthTest {
+
+    @Test
+    public void whenFirstSingleton() {
+        TrackerSingleFourth tracker1 = TrackerSingleFourth.getInstance();
+        TrackerSingleFourth tracker2 = TrackerSingleFourth.getInstance();
+
+        assertThat(tracker1, is(tracker2));
+    }
+}
