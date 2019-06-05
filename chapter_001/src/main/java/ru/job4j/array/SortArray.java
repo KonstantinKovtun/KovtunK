bcc726b8bcf66b02105bf097f3e1a83fa1eea704
KonstantinKovtun <kovtun.kostya@gmail.com>
2019-06-06 00:53:56 +0300

    Контрольные вопросы часть 1. Базовый синтаксис.[#47975]

--- chapter_001/src/main/java/ru/job4j/array/SortArray.java
+++ chapter_001/src/main/java/ru/job4j/array/SortArray.java
@@ -1,6 +1,7 @@
 package ru.job4j.array;
 
 import java.util.Arrays;
+
 /**
  * Class SortArray creates new array and then sort it.
  * @author Kovtun Konstantin (kovtun.kostya@gmail.com)
@@ -9,26 +10,63 @@ import java.util.Arrays;
  */
 public class SortArray {
     /**
-     * insertElements. Fill the elements of array.
-     * @param array1, name of array.
-     * @param array2, name of array.
-     * @return all return sorted array.
+     *
+     * @param buffer1 Массив для сортировки.
+     * @param buffer2 Буфер. Размер должен быть равен размеру buffer1.
+     * @param startIndex Начальный индекс в buffer1 для сортировки.
+     * @param endIndex Конечный индекс в buffer1 для сортировки.
+     * @return
      */
-    public int[] insertElements(int[] array1, int[] array2) {
+    public static int[] mergesortInner(int[] buffer1, int[] buffer2, int startIndex, int endIndex) {
+        if (startIndex >= endIndex - 1) {
+            return buffer1;
+        }
 
-        int[] all = new int[array1.length + array2.length];
+        int middle = startIndex + (endIndex - startIndex) / 2;
+        int[] sorted1 = mergesortInner(buffer1, buffer2, startIndex, middle);
+        int[] sorted2 = mergesortInner(buffer1, buffer2, middle, endIndex);
 
-        System.arraycopy(array1, 0, all, 0, array1.length);
-        System.arraycopy(array2, 0, all, array1.length, array2.length);
+        int index1 = startIndex;
+        int index2 = middle;
+        int destIndex = startIndex;
+        int[] result = sorted1 == buffer1 ? buffer2 : buffer1;
 
-        return all;
+        while (index1 < middle && index2 < endIndex) {
+            result[destIndex++] = sorted1[index1] < sorted2[index2]
+                    ? sorted1[index1++] : sorted2[index2++];
+        }
+        while (index1 < middle) {
+            result[destIndex++] = sorted1[index1++];
+        }
+        while (index2 < endIndex) {
+            result[destIndex++] = sorted2[index2++];
+        }
+        return result;
+    }
+
+    public static int[] mergesort(int[] array1) {
+        int[] buffer1 = Arrays.copyOf(array1, array1.length);
+        int[] buffer2 = new int[array1.length];
+        int[] result = mergesortInner(buffer1, buffer2, 0, array1.length);
+
+        return result;
+    }
+
+    public static int[] insertElements(int[] array1, int[] array2) {
+        int[] array3 = new int[array1.length + array2.length];
+
+        System.arraycopy(array1, 0, array3, 0, array1.length);
+        System.arraycopy(array2, 0, array3, array1.length, array2.length);
+
+        return mergesort(array3);
     }
 
     public static void main(String[] args) {
-        int[] ara1 = {7, 6};
-        int[] ara2 = {0, 3};
+        int[] ar1 = {31, 25, -2, 21, -43};
+        int[] ar2 = {3, -99, 0};
 
-        SortArray sortArray = new SortArray();
-        System.out.println(Arrays.toString(sortArray.insertElements(ara1, ara2)));
+        System.out.println(Arrays.toString(insertElements(ar1, ar2)));
     }
 }
+
+
