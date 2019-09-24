03853163e93d9b0e96a0362b1b36b4dd12cda95a
KonstantinKovtun <kovtun.kostya@gmail.com>
2019-09-24 23:17:42 +0300

    4. Конвертация двумерного массива в ArrayList [#48580]

--- chapter_003/src/main/java/ru/job4j/list/ConvertMatrix2List.java
+++ chapter_003/src/main/java/ru/job4j/list/ConvertMatrix2List.java
@@ -3,9 +3,27 @@ package ru.job4j.list;
 import java.util.ArrayList;
 import java.util.List;
 
+
+/**
+ * Class ConvertMatrix2List. This class describes behavior of returning array in List.
+ * @author Kovtun Konstantin (kovtun.kostya@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class ConvertMatrix2List {
+    /**
+     * Method toList. The method returns the array of List.
+     * @param array contains the two arrays.
+     * @return the List with elements.
+     */
     public List<Integer> toList(int[][] array) {
         List<Integer> list = new ArrayList<>();
+
+        for (int[] i : array) {
+            for (int j : i) {
+                list.add(j);
+            }
+        }
         return list;
     }
 }
