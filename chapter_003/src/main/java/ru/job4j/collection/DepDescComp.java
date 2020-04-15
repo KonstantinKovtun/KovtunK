fb4d4fc5f99c2efd1c0173d8dc7d3bd21bcdbfab
KonstantinKovtun <kovtun.kostya@gmail.com>
2020-04-15 18:24:55 +0300

    Отсортировать департаменты [#34352]

--- chapter_003/src/main/java/ru/job4j/collection/DepDescComp.java
+++ chapter_003/src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,17 +5,6 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-//        public int compare(House h1, House h2) {
-//            if (h1.price == h2.price) {
-//                return 0;
-//            }
-//            if (h1.price > h2.price) {
-//                return 1;
-//            }
-//            else {
-//                return -1;
-//            }
- //       if (o1.
-        return 0;
+        return o1.compareTo(o2);
     }
 }
--- chapter_003/src/main/java/ru/job4j/collection/Departments.java
+++ chapter_003/src/main/java/ru/job4j/collection/Departments.java
@@ -1,42 +1,31 @@
 package ru.job4j.collection;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class Departments {
 
     public static List<String> fillGaps(List<String> deps) {
 
         HashSet<String> tmp = new HashSet<>();
-        String name = "";
 
         for (String value : deps) {
             String start = "";
             for (String el : value.split("/")) {
-               // start = start.isBlank() ? el : start + "/" + el;
-                 //tmp.add(start + el);
+                start = start.isBlank() ? el : start + "/" + el;
                 tmp.add(start);
             }
         }
 
-        for (int i = 0; i < tmp.size(); i++) {
-            if (!tmp.contains("k1")) {
-                tmp.add("k1");
-            } else if (!tmp.contains("k1/sk1")) {
-                tmp.add("k1/sk1");
-            } else if (tmp.contains("sk1")) {
-                tmp.remove("sk1");
-            }
-        }
-        sortAsc(new ArrayList<>(tmp));
+        sortAsc((new ArrayList<>(tmp)));
 
         return new ArrayList<>(tmp);
     }
 
     public static void sortAsc(List<String> orgs) {
+        Collections.sort(orgs, new DepDescComp());
     }
 
     public static void sortDesc(List<String> orgs) {
+        Collections.reverseOrder();
     }
 }
