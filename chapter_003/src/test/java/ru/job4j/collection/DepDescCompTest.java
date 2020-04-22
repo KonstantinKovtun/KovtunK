aa3ca24c8c3dbf90826e4daf72e25d46536e7c5a
KonstantinKovtun <kovtun.kostya@gmail.com>
2020-04-22 09:10:56 +0300

    Отсортировать департаменты [#34352]

--- chapter_003/src/test/java/ru/job4j/collection/DepDescCompTest.java
+++ chapter_003/src/test/java/ru/job4j/collection/DepDescCompTest.java
@@ -6,23 +6,35 @@ import static org.hamcrest.Matchers.lessThan;
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.greaterThan;
 
+/**
+ * Test class DepartmentsTest
+ *
+ * @author Kovtun Konstantin (kovtun.kostya@gmai.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class DepDescCompTest {
-
+    /**
+     * Test compare.
+     */
     @Test
     public void compare() {
-//        int rsl = new DepDescComp().compare(
-//                "K2/SK1/SSK2",
-//                "K2/SK1/SSK1"
-//        );
-//        assertThat(rsl, greaterThan(0));
+        int rsl = new DepDescComp().compare(
+                "K2/SK1/SSK2",
+                "K2/SK1/SSK1"
+        );
+        assertThat(rsl, greaterThan(0));
     }
 
+    /**
+     * Test whenUpDepartmentGoBefore.
+     */
     @Test
     public void whenUpDepartmentGoBefore() {
-//        int rsl = new DepDescComp().compare(
-//                "K2",
-//                "K2/SK1"
-//        );
-//        assertThat(rsl, lessThan(0));
+        int rsl = new DepDescComp().compare(
+                "K2",
+                "K2/SK1"
+        );
+        assertThat(rsl, lessThan(0));
     }
 }
