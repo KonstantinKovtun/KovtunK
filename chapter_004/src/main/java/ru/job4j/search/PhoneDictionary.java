4d80eca9526bcb02962137d185d18c3c81fac38d
KonstantinKovtun <kovtun.kostya@gmail.com>
2020-05-21 01:07:43 +0300

    6. Функции высшего порядка[24255#273345]

--- chapter_004/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_004/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -24,55 +24,37 @@ public class PhoneDictionary {
      */
     public List<Person> find(String key) {
 
-//        Predicate<Person> combine = k -> {
-//            k.getName().contains(key);
-//            k.getSurname().contains(key);
-//            k.getPhone().contains(key);
-//            k.getPhone().contains(key);
-//            k.getAddress().contains(key);
-//
-//        };
+        Predicate<Person> combine = k -> {
+            if (k.getName().contains(key)
+            || k.getSurname().contains(key)
+            || k.getPhone().contains(key)
+            || k.getPhone().contains(key)
+            || k.getAddress().contains(key)) {
+                return true;
+            }
+            return false;
+        };
 
         ArrayList<Person> result = new ArrayList<>();
 
-//        for (Person person : persons) {
-//            if (combine.test(person)) {
-//                result.add(person);
-//            }
-//        }
+        for (Person person : persons) {
+            if (combine.test(person)) {
+                result.add(person);
+            }
+        }
 
-//        for (int i = 0; i < result.size(); i++) {
-//            if (result.size() == 0) {
-//                System.out.println("Collections empty!!!!!!!");
-//            } else {
-//                System.out.print(result.get(i).getName());
-//                System.out.print(result.get(i).getSurname());
-//                System.out.print(result.get(i).getPhone());
-//                System.out.print(result.get(i).getAddress());
-//            }
-//            System.out.println();
-//        }
+        for (int i = 0; i < result.size(); i++) {
+            if (result.size() == 0) {
+                System.out.println("Collections empty!!!!!!!");
+            } else {
+                System.out.print(result.get(i).getName() + " ");
+                System.out.print(result.get(i).getSurname() + " ");
+                System.out.print(result.get(i).getPhone() + " ");
+                System.out.print(result.get(i).getAddress() + " ");
+            }
+            System.out.println();
+        }
 
         return result;
-    }
-
-    /**
-     * Main
-     * @param args - args.
-     */
-    public static void main(String[] args) {
-
-//        PhoneDictionary persn = new PhoneDictionary();
-//
-//        persn.add(new Person("name1", "surname1", "phone1", "adderss1"));
-//        persn.add(new Person("name20", "surname2", "phone20", "adderss20"));
-//        persn.add(new Person("name30", "surname30", "phone5", "adderss50"));
-//        persn.add(new Person("name1", "surname40", "phone40", "adderss+100500"));
-//        persn.add(new Person("name70", "surname70", "phone70", "adderss70"));
-//
-//        persn.find("name1");
-//        persn.find("surname2");
-//        persn.find("phone5");
-//        persn.find("adderss+100500");
     }
 }
